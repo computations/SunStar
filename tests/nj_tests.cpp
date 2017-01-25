c1143a7e7a2087cf35bda94d39ed548fbc377479
Ben Bettisworth <bwbettisworth@alaska.edu>
2017-01-25 09:41:33 -0900

    fixed some tests for NJ.

--- tests/nj_tests.cpp
+++ tests/nj_tests.cpp
@@ -12,20 +12,20 @@ TEST_CASE("nj with simple distance table", "[nj]"){
 }
 
 TEST_CASE("nj with larger distance table", "[nj]"){
-    std::vector<float> d = {0.0, 1.0, 2.0,
+    std::vector<float> d = {0.0, 1.0, 1.0,
                             1.0, 0.0, 1.0,
-                            2.0, 1.0, 0.0};
+                            1.0, 1.0, 0.0};
     std::vector<std::string> l {"a", "b", "c"};
     nj_t n(d,l);
-    REQUIRE(n.get_tree().to_string() == "((a,b),c);");
+    REQUIRE(n.get_tree().to_string() == "(a:0.5,b:0.5,c:0.5);");
 }
 
-TEST_CASE("nj with largest distance table", "[nj]"){
-    std::vector<float> d = {0.0, 1.0, 2.0, 3.0,
-                            1.0, 0.0, 1.0, 2.0,
-                            2.0, 1.0, 0.0, 1.0, 
-                            3.0, 2.0, 1.0, 0.0};
+TEST_CASE("nj with largest distance table", "[nj][regression]"){
+    std::vector<float> d = {0.0, 1.0, 2.5, 2.5,
+                            1.0, 0.0, 2.5, 2.5,
+                            2.5, 2.5, 0.0, 1.0,
+                            2.5, 2.5, 1.0, 0.0};
     std::vector<std::string> l {"a", "b", "c", "d"};
     nj_t n(d,l);
-    REQUIRE(n.get_tree().to_string() == "((a,b),c);");
+    REQUIRE(n.get_tree().to_string() == "(a:0.5,b:0.5,(d:0.5,c:0.5):1.5);");
 }
