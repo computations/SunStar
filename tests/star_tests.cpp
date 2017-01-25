2d16a14624472053b51111aee40bb08921fd00fa
Ben Bettisworth <bwbettisworth@alaska.edu>
2017-01-25 14:10:21 -0900

    wrote some more star tests

--- tests/star_tests.cpp
+++ tests/star_tests.cpp
@@ -10,3 +10,27 @@ TEST_CASE("star, one tree","[star]"){
     star_tree.sort();
     REQUIRE(star_tree.to_string()== t1);
 }
+
+TEST_CASE("star, two cloned trees", "[star]"){
+    std::string t1="(a:1.0,b:1.0);";
+    std::vector<std::string> vt;
+    vt.push_back(t1);
+    vt.push_back(t1);
+    star_t s(vt);
+    auto star_tree = s.get_tree();
+    star_tree.sort();
+    REQUIRE(star_tree.to_string()== t1);
+}
+
+TEST_CASE("star, two different trees", "[star]"){
+    std::string t1 = "(a:1.0,b:1.0);";
+    std::string t2 = "(a:2.0,b:0.5);";
+    std::vector<std::string> vt;
+    vt.push_back(t1);
+    vt.push_back(t2);
+    star_t s(vt);
+    auto star_tree = s.get_tree();
+    star_tree.set_weights([](size_t d) -> float {return 1.0;});
+    star_tree.sort();
+    REQUIRE(star_tree.to_string()== t1);
+}
