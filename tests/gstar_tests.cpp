21f9759b27949c71519a56ae416e4843d7bac291
Ben Bettisworth <bwbettisworth@alaska.edu>
2017-02-21 12:26:22 -0900

    one more test, just in case

--- tests/gstar_tests.cpp
+++ tests/gstar_tests.cpp
@@ -6,9 +6,13 @@ TEST_CASE("gstar, functionality test","[gstar]"){
     auto c = gstar({ns});
 }
 
-#include <iostream>
 TEST_CASE("gstar, functionality test, larger class", "[gstar]"){
     std::string ns1 = "(a,(b,(c,d)));";
     std::string ns2 = "((a,b),(c,d));";
     auto c = gstar({ns1,ns2});
+}
+
+TEST_CASE("gstar, functional test, larger class", "[gstar]"){
+    std::string ns = "(((a,b),(c,d)),(((e,f),g),h));";
+    auto c = gstar({ns});
 }
